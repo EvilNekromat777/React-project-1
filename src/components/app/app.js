506a4488a1ddb265d9e9a10f300e9fd7ccb38424
unknown <evil-nekromat@mail.ru>
2022-04-07 12:07:41 +0300

    реализовали удаление при клике на корзину

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,28 +8,46 @@ import EmployersAddForm from '../employers-add-form/employers-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Ivan P.', salary: 1200, increase: false, id:1},
+                {name: 'John C.', salary: 800, increase: false, id:2},
+                {name: 'Alex M.', salary: 3000, increase: true, id:3},
+                {name: 'Evil N.', salary: 5000, increase: false, id:4}
+            ]
+        }
+    }
 
-    const data = [
-        {name: 'Ivan P.', salary: 1200, increase: false, id:1},
-        {name: 'John C.', salary: 800, increase: false, id:2},
-        {name: 'Alex M.', salary: 3000, increase: true, id:3},
-        {name: 'Evil N.', salary: 5000, increase: false, id:4}
-    ];
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            
+            return {
+                data: data.filter(item => item.id !== id) // с помощью метода filter создаем новый массив данных, кроме элемента с выбранным id
+            }
+        })
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
+    render() {
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployersList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployersAddForm/>
             </div>
-
-            <EmployersList data={data}/>
-            <EmployersAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
